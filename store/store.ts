a22517d797eeffec49490be6f09dbfd8d24cb783
DRidleyS <dorianridley@gmail.com>
2026-01-05 07:30:34 -0800

    for-you page

--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ui from "./uiSlice";
+import auth from "./authSlice";
 
-export const store = configureStore({ reducer: { ui } });
+export const store = configureStore({ reducer: { ui, auth } });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
